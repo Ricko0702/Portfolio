e141e05cb7efc68f441ef4ee03c70d3b18551596
Student Rick van Assen <rassen0134@student.landstede.nl>
2021-10-15 09:15:05 +0200

    .

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -9,8 +9,36 @@
     <link rel="icon" href="images/logo.png">
 </head>
 <body>
+<header>
+        <nav>
+            <div class="logo"> <img src="/images/logo.png" alt="logo"></div>
+                
 
+                <div class="home">
+                        <a href="/"><h3>home</h3></a>
+                </div>
+                <div class="dropmenu">
+                    <div class="drop">
+                        <a href="/projecten"><h3>projecten</h3></a>
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="/projecten/deze-website">Deze website</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="overmij">
+                    <a href="/overmij"><h3>overmij</h3></a>
+                </div>
 
+                <div id=taal>
+        
+                <a href="/en/home">
+                <img src="/images/uk.png" alt="">
+                </a>
+            </div>
+        </nav>
+    </header>
 
 @yield('body')
 
