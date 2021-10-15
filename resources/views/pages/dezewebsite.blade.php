99ced96d6370fe0353cd03d7ab1e5214842aa1d2
Student Rick van Assen <rassen0134@student.landstede.nl>
2021-10-15 13:53:16 +0200

    ,

--- resources/views/pages/dezewebsite.blade.php
+++ resources/views/pages/dezewebsite.blade.php
@@ -13,10 +13,6 @@
 </div>
 
 <div id="dezewebsitetekst">
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu convallis nulla, a volutpat ex. Sed egestas, massa vitae hendrerit posuere, orci risus posuere ligula, vel pretium neque arcu vitae arcu. Nulla feugiat turpis et enim gravida, laoreet tempor urna molestie. Suspendisse sed egestas purus, et efficitur dui. Fusce tincidunt eget magna id varius. Aenean pharetra ipsum eu convallis rhoncus. Curabitur in lobortis augue. Aliquam tristique consectetur nisl nec tempus. Donec ut risus quam. Duis dignissim, mauris ac tincidunt ullamcorper, justo sapien aliquam nisi, a volutpat quam sem in dolor. Phasellus tincidunt nisi et mi semper, vitae imperdiet nisi semper. Aliquam erat volutpat. Sed ac lectus lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque at velit vel urna ornare dictum.
-
-Sed venenatis dignissim metus quis tincidunt. Vivamus auctor tristique mauris quis ornare. Sed varius quis elit eu gravida. Sed lobortis mattis mattis. Morbi quis fringilla neque. Quisque condimentum posuere consectetur. Integer tempor vitae lectus a facilisis. Quisque pharetra lacus nisl, sagittis blandit leo porta ut. Proin non dui luctus, commodo dui quis, interdum magna. Sed augue mi, lobortis ac augue a, tincidunt tempor tortor. Integer pretium erat nibh, vel congue dolor venenatis sit amet. Ut varius arcu quis egestas hendrerit. Suspendisse leo turpis, bibendum ut tempor in, malesuada nec enim. Aliquam erat velit, efficitur vel tortor ut, venenatis consequat nunc. Duis vehicula tempus sem quis finibus. Sed sed maximus dui.
-
 
 
 </div>
